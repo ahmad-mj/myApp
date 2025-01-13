eb0c4c8d4f9d742e475e650707f0e2e72e9e8a68
ahmad <ahmad_almujawer@icloud.com>
2025-01-13 17:57:22 +0100

    Add properties to domain class

--- grails-app/domain/org/grails/guides/Vehicle.groovy
+++ grails-app/domain/org/grails/guides/Vehicle.groovy
@@ -1,7 +1,15 @@
 package org.grails.guides
 
 class Vehicle {
+    // Properties will be used to create columns in the database (assuming a relational database is used)
+    String name
+    String make
+    String model
 
     static constraints = {
+    // Constraints are used to enforce valid data in each field
+        name maxSize: 255
+        make inList: ['Ford', 'Chevrolet', 'Nissan']
+        model nullable: true
     }
 }
