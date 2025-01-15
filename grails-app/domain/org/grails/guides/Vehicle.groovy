120e1e32837033fdc58951f8f72df03e1e178198
ahmad <ahmad_almujawer@icloud.com>
2025-01-15 17:43:08 +0100

    Update Vehicle to use the new classes

--- grails-app/domain/org/grails/guides/Vehicle.groovy
+++ grails-app/domain/org/grails/guides/Vehicle.groovy
@@ -1,15 +1,16 @@
 package org.grails.guides
 
+@SuppressWarnings('GrailsDomainReservedSqlKeywordName')
 class Vehicle {
     // Properties will be used to create columns in the database (assuming a relational database is used)
+    Integer year   
     String name
     String make
     String model
 
     static constraints = {
     // Constraints are used to enforce valid data in each field
+        year min: 1900
         name maxSize: 255
-        make inList: ['Ford', 'Chevrolet', 'Nissan']
-        model nullable: true
     }
 }
