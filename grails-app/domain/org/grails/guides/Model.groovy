f9097eeab5d0f53cd478b0540729a54a1e7e788f
ahmad <ahmad_almujawer@icloud.com>
2025-01-21 17:21:43 +0100

    Update nme property

--- grails-app/domain/org/grails/guides/Model.groovy
+++ grails-app/domain/org/grails/guides/Model.groovy
@@ -2,11 +2,12 @@ package org.grails.guides
 
 class Model {
 
-    Sting name
+    String name
 
     static belongsTo = [ make: Make ] // belongsTo is property GORM uses to determine associations between domain classes
     
     static constraints = {
+        name blank: false, unique:true
     }
 
     String toString() {
