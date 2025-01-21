1baef1abb68e230528c6f7e9056975411c684e7b
ahmad <ahmad_almujawer@icloud.com>
2025-01-21 17:24:29 +0100

    Define properties and Adjust constrains

--- grails-app/domain/org/grails/guides/Vehicle.groovy
+++ grails-app/domain/org/grails/guides/Vehicle.groovy
@@ -1,16 +1,20 @@
 package org.grails.guides
+import org.grails.guides.Make
+import org.grails.guides.Model
 
 @SuppressWarnings('GrailsDomainReservedSqlKeywordName')
 class Vehicle {
     // Properties will be used to create columns in the database (assuming a relational database is used)
     Integer year   
     String name
-    String make
-    String model
+    Make make
+    Model model
 
     static constraints = {
     // Constraints are used to enforce valid data in each field
         year min: 1900
         name maxSize: 255
+        make nullable: false
+        model nullable: false
     }
 }
