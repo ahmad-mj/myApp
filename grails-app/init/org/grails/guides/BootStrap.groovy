af9985e3c3ecfff0e407cb4650aaa91342323da9
ahmad <ahmad_almujawer@icloud.com>
2025-01-21 17:32:37 +0100

    Update method calls and assignment

--- grails-app/init/org/grails/guides/BootStrap.groovy
+++ grails-app/init/org/grails/guides/BootStrap.groovy
@@ -1,12 +1,26 @@
 package org.grails.guides
 import org.grails.guides.services.MakeService
-import org.grails.guides.Make
+import org.grails.guides.services.ModelService
+import org.grails.guides.services.VehicleService
 
 class BootStrap {
 
     MakeService makeService
+    ModelService modelService
+    VehicleService vehicleService
     def init = { servletContext ->
-    def toyota= makeService.saveMake('Toyota')
+// Create Makes
+    def toyota = makeService.saveMake('Toyota')
+    def volkswagen = makeService.saveMake('Volkswagen')
+// Create Models
+    def titan = modelService.saveModel('Titan', toyota)
+    def leaf = modelService.saveModel('Leaf', toyota)
+    def windstar = modelService.saveModel('Windstar', volkswagen)
+// Create Vehicles
+    vehicleService.save('Pickup', toyota, titan, 2020)
+    vehicleService.save('Kombi', volkswagen, windstar, 2021)
+    vehicleService.save('Sedan', toyota, leaf, 2019)
+
     }
     def destroy = {
     }
