fa283e052d55b301f0029566ae64b08b3b0c1a54
ahmad <ahmad_almujawer@icloud.com>
2025-02-24 20:27:36 +0100

    Inject and Use the Service in Controller

--- grails-app/controllers/org/grails/guides/HomeController.groovy
+++ grails-app/controllers/org/grails/guides/HomeController.groovy
@@ -1,12 +1,14 @@
 package org.grails.guides
+import org.grails.guides.services.VehicleService
 
 class HomeController {
 
+    VehicleService vehicleService
 
    def index() {
         respond([name: session.name ?: 'User', vehicleTotal: Vehicle.count()]) 
 
-        def vehicles = Vehicle.list()
+        def vehicles = vehicleService.listAllVehicles()
         render(view:'index', model:[vehicleList: vehicles])
     }
 
@@ -15,6 +17,19 @@ class HomeController {
 
         flash.message = "Name has been updated" 
 
+        redirect action: 'index' 
+    }
+    def save() {
+       String name = params.name
+       String makeName = params.make
+       String modelName = params.model
+       Integer year = params.int('year')
+
+       def make = Make.findByName(makeName) ?: new Make(name: makeName).save()
+       def model = Model.findByName(modelName) ?: new Model(name: modelName).save()
+
+       vehicleService.save(name, make, model, year)
+        
         redirect action: 'index' 
     }
 
