1ab708c579b07a581f1bb83a35f2f3d1f1447ab1
ahmad <ahmad_almujawer@icloud.com>
2025-02-25 15:58:48 +0100

    Modify  the save() method in to accept String values

--- grails-app/services/VehicleService.groovy
+++ grails-app/services/VehicleService.groovy
@@ -9,16 +9,22 @@ class VehicleService {
     def getVehicleDetails() {
         return "Example Vehicle Details"
     }
-    def save(String name, Make make, Model model, Integer year) {
-        def vehicle = new Vehicle(name: name, make: make, model: model, year: year)
+
+    def save(String name, String makeName, String modelName, Integer year) {
+            def make = Make.findByName(makeName) ?: new Make(name: makeName).save(flush: true)
+            def model = Model.findByNameAndMake(modelName, make) ?: new Model(name: modelName, make: make).save(flush: true)
+            def vehicle = new Vehicle(name: name, make: make, model: model, year: year)
+
         if (!vehicle.save(flush: true)) {
             throw new RuntimeException("Failed to save vehicle: ${vehicle.errors}")
         }
         return vehicle
-    }
+     }
+
      def listAllVehicles() {
         Vehicle.list()
     }
+    
      def getVehicle(Long id) {
         Vehicle.get(id)
     }
